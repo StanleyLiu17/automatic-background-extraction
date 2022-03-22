59f305cb7c8d16518c2a4dd8018b0fdc140567d3
Slevire <syliu1@asu.edu>
2022-03-22 14:25:40 -0700

    Fixed segmentation prediction to use SegNet and cuda. Altered prediction so it predicts one given image rather than images in a test folder

--- src/segmentor.py
+++ src/segmentor.py
@@ -1,4 +1,14 @@
 import numpy as np
+import torch
+from torch.autograd import Variable
+from skimage import io
+from networks import SegNet
+from utils import sliding_window, grouper
+
+net = SegNet()
+WINDOW_SIZE = (256, 256) # Patch size
+BATCH_SIZE = 10 # Number of samples in a mini-batch
+N_CLASSES = 6
 
 palette = {0 : (255, 255, 255), # Impervious surfaces (white)
            1 : (0, 0, 255),     # Buildings (blue)
@@ -44,36 +54,30 @@ def remove_patch_og(real_img,mask):
     og_data[idx] = 255
     return og_data
 
-def segmentor(net, test_ids, stride=WINDOW_SIZE[0], batch_size=BATCH_SIZE, window_size=WINDOW_SIZE):
-    # Use the network on the test set
-    test_images = (1 / 255 * np.asarray(io.imread(DATA_FOLDER.format(id)), dtype='float32') for id in test_ids)
-    all_preds = []
+def segmentor(net, img, stride=WINDOW_SIZE[0], batch_size=BATCH_SIZE, window_size=WINDOW_SIZE):
     
     # Switch the network to inference mode
+    net.cuda()
     net.eval()
     
-    for img in test_images:
-        pred = np.zeros(img.shape[:2] + (N_CLASSES,))
+    pred = np.zeros(img.shape[:2] + (N_CLASSES,))
 
-        for i, coords in enumerate(grouper(batch_size, sliding_window(img, step=stride, window_size=window_size))):
+    for i, coords in enumerate(grouper(batch_size, sliding_window(img, step=stride, window_size=window_size))):
                     
-            with torch.no_grad():
-                # Build the tensor
-                image_patches = [np.copy(img[x:x+w, y:y+h]).transpose((2,0,1)) for x,y,w,h in coords]
-                image_patches = np.asarray(image_patches)
-                image_patches = Variable(torch.from_numpy(image_patches).cuda())
+        with torch.no_grad():
+            # Build the tensor
+            image_patches = [np.copy(img[x:x+w, y:y+h]).transpose((2,0,1)) for x,y,w,h in coords]
+            image_patches = np.asarray(image_patches)
+            image_patches = Variable(torch.from_numpy(image_patches).cuda())
                 
-                # Do the inference
-                outs = net(image_patches)
-                outs = outs.data.cpu().numpy()
+            # Do the inference
+            outs = net(image_patches)
+            outs = outs.data.cpu().numpy()
                 
-                # Fill in the results array
-                for out, (x, y, w, h) in zip(outs, coords):
-                    out = out.transpose((1,2,0))
-                    pred[x:x+w, y:y+h] += out
-                del(outs)
+            # Fill in the results array
+            for out, (x, y, w, h) in zip(outs, coords):
+                out = out.transpose((1,2,0))
+                pred[x:x+w, y:y+h] += out
+            del(outs)
 
-        pred = np.argmax(pred, axis=-1)
-        all_preds.append(pred)
-    
-    return all_preds
+    return np.argmax(pred, axis=-1)
