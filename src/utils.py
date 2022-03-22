c6668439930f1b2db80924632248e87fd816be07
Slevire <syliu1@asu.edu>
2022-03-22 14:10:34 -0700

    added needed utility functions and imports

--- src/utils.py
+++ src/utils.py
@@ -5,12 +5,23 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import torch
+import torch.nn.functional as F
+import itertools
 
+palette = {0 : (255, 255, 255), # Impervious surfaces (white)
+           1 : (0, 0, 255),     # Buildings (blue)
+           2 : (0, 255, 255),   # Low vegetation (cyan)
+           3 : (0, 255, 0),     # Trees (green)
+           4 : (255, 255, 0),   # Cars (yellow)
+           5 : (255, 0, 0),     # Clutter (red)
+           6 : (0, 0, 0)}       # Undefined (black)
+
+invert_palette = {v: k for k, v in palette.items()}
 
 def create_dir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
-
 
 def create_mask(width, height, mask_width, mask_height, x=None, y=None):
     mask = np.zeros((height, width))
@@ -18,7 +29,6 @@ def create_mask(width, height, mask_width, mask_height, x=None, y=None):
     mask_y = y if y is not None else random.randint(0, height - mask_height)
     mask[mask_y:mask_y + mask_height, mask_x:mask_x + mask_width] = 1
     return mask
-
 
 def stitch_images(inputs, *outputs, img_per_row=2):
     gap = 5
@@ -39,7 +49,6 @@ def stitch_images(inputs, *outputs, img_per_row=2):
 
     return img
 
-
 def imshow(img, title=''):
     fig = plt.gcf()
     fig.canvas.set_window_title(title)
@@ -47,13 +56,10 @@ def imshow(img, title=''):
     plt.imshow(img, interpolation='none')
     plt.show()
 
-
 def imsave(img, path):
     im = Image.fromarray(img.cpu().numpy().astype(np.uint8).squeeze())
     
     im.save(path)
-
-
 class Progbar(object):
     """Displays a progress bar.
 
@@ -215,3 +221,67 @@ class Progbar(object):
 
     def add(self, n, values=None):
         self.update(self._seen_so_far + n, values)
+
+def convert_to_color(arr_2d, palette=palette):
+    """ Numeric labels to RGB-color encoding """
+    arr_3d = np.zeros((arr_2d.shape[0], arr_2d.shape[1], 3), dtype=np.uint8)
+
+    for c, i in palette.items():
+        m = arr_2d == c
+        arr_3d[m] = i
+
+    return arr_3d
+
+def convert_from_color(arr_3d, palette=invert_palette):
+    """ RGB-color encoding to grayscale labels """
+    arr_2d = np.zeros((arr_3d.shape[0], arr_3d.shape[1]), dtype=np.uint8)
+
+    for c, i in palette.items():
+        m = np.all(arr_3d == np.array(c).reshape(1, 1, 3), axis=2)
+        arr_2d[m] = i
+
+    return arr_2d
+
+def get_random_pos(img, window_shape):
+    """ Extract of 2D random patch of shape window_shape in the image """
+    w, h = window_shape
+    W, H = img.shape[-2:]
+    x1 = random.randint(0, W - w - 1)
+    x2 = x1 + w
+    y1 = random.randint(0, H - h - 1)
+    y2 = y1 + h
+    return x1, x2, y1, y2
+
+def CrossEntropy2d(input, target, weight=None, size_average=True):
+    """ 2D version of the cross entropy loss """
+    dim = input.dim()
+    if dim == 2:
+        return F.cross_entropy(input, target, weight, size_average)
+    elif dim == 4:
+        output = input.view(input.size(0),input.size(1), -1)
+        output = torch.transpose(output,1,2).contiguous()
+        output = output.view(-1,output.size(2))
+        target = target.view(-1)
+        return F.cross_entropy(output, target,weight, size_average)
+    else:
+        raise ValueError('Expected 2 or 4 dimensions (got {})'.format(dim))
+
+def sliding_window(top, step=10, window_size=(20,20)):
+    """ Slide a window_shape window across the image with a stride of step """
+    for x in range(0, top.shape[0], step):
+        if x + window_size[0] > top.shape[0]:
+            x = top.shape[0] - window_size[0]
+        for y in range(0, top.shape[1], step):
+            if y + window_size[1] > top.shape[1]:
+                y = top.shape[1] - window_size[1]
+            yield x, y, window_size[0], window_size[1]
+
+def grouper(n, iterable):
+    """ Browse an iterator by chunk of n elements """
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            return
+        yield chunk
+
