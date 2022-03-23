1083bc2536c824d989a7f867222eb9bc2ebdfba4
Slevire <syliu1@asu.edu>
2022-03-22 22:02:30 -0700

    whitespace changes

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ def load_config(mode=None):
     r"""loads model config
 
     """
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', '--checkpoints', type=str, default='./checkpoints', help='model checkpoints path (default: ./checkpoints)')
     parser.add_argument('--model', type=int, choices=[1, 2, 3, 4], help='1: edge model, 2: inpaint model, 3: edge-inpaint model, 4: joint model')
