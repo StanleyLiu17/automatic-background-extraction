835499e2e6ea9f0f1ebfc2b7d56e91e1db7d5482
Slevire <syliu1@asu.edu>
2022-03-30 14:56:07 -0700

    Began image preprocessing script

---
+++ src/image_proc.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+from PIL import Image, ImageChops
+import image_slicer, os, shutil
+
+def pad_n_slice(image):
+    arr = np.asarray(image)
+
+def trim(im):
+    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
