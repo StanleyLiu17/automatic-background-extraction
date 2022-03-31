5b2148c0af5357f01e84a45285b174bc0812c421
Slevire <syliu1@asu.edu>
2022-03-30 19:46:07 -0700

    update

--- src/image_proc.py
+++ src/image_proc.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
 from PIL import Image, ImageChops
-import image_slicer, os, shutil
+import image_slicer, os, shutil, sklearn
 
 def pad_n_slice(image):
     arr = np.asarray(image)
+
 
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
@@ -13,3 +14,4 @@ def trim(im):
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
+
