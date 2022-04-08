c0adb60e8b5cc2e0a86fe1cd1c40a13606662b78
Slevire <syliu1@asu.edu>
2022-04-07 20:39:34 -0700

    Created two sets of slice and stitch methods. One set is meant to be used within a loop to handle a directory of images. The other set does this directly in the method

--- src/image_proc.py
+++ src/image_proc.py
@@ -1,34 +1,67 @@
 from PIL import Image, ImageChops
-import os
+import os, re
 from skimage import io
 from empatches import EMPatches
 from ast import literal_eval
 from natsort import natsorted
 
-def slice_img(img):
+emp = EMPatches()
+
+def slice_img(img, i):
     
     image = io.imread(img)
-    emp = EMPatches()
     patches, indices = emp.extract_patches(image, patchsize=256, overlap=0.1)
 
     with open('indices.txt', 'a+') as f:
         f.write(str(indices))
     
-    for i in range(len(patches)):
+    for j in range(len(patches)):
         im = Image.fromarray(patches[i])
-        im.save(f"./Patches/{i}_{os.path.basename(img)}")
-
+        im.save(f"./Patches/{i}_{j}_{os.path.basename(img)}")
+            
 def stitch_patches(patch_dir, out_dir, i):
     
-    emp = EMPatches()
     patches = [io.imread(patch) for patch in natsorted([os.path.join(patch_dir, patch) for patch in os.listdir(patch_dir)])]
     
     with open('indices.txt') as f:
-        indices = [literal_eval(index) for index in f.readlines()]
+        indices = [literal_eval(f.readlines[i])]
     
-    im = emp.merge_patches(patches, indices) 
-    image = Image.fromarray(im)
+    image = Image.fromarray(emp.merge_patches(patches, indices))
     image.save(f"{out_dir}/result_{i}.png")
+
+def slice_img_dir(image_paths):
+    
+    with open('indices.txt', 'a+') as f:
+        for i, image_path in enumerate(len(image_paths)):
+            
+            image = io.imread(image_path)    
+            patches, indices = emp.extract_patches(image, patchsize=256, overlap=0.1)
+            f.write(str(indices))
+            
+            for j in range(len(patches)):
+                im = Image.fromarray(patches[i])
+                im.save(f"./Patches/{i}_{j}_{os.path.basename(image_path)}")
+                
+def stitch_patches_dir(out_dir):
+    
+    all_patch_paths = natsorted(os.path.join('./Patches', patch) for patch in os.listdir('./Patches'))
+    
+    for i, patch_path in enumerate(len(all_patch_paths)):
+        patch_counter = 0
+        
+        while re.search(r'\d+', os.path.basename(patch_path)).group() == i:
+            patch_counter += 1
+        
+        patch_paths = all_patch_paths[0:patch_counter + 1] # Get patche paths belonging to one image
+        all_patch_paths = all_patch_paths[patch_counter + 2:] # Remove patch paths from list
+
+        patches = [io.imread(patch) for patch in patch_paths]
+        
+        with open('indices.txt') as f:
+            indices = [literal_eval(f.readlines[i])]
+        
+        image = Image.fromarray(emp.merge_patches(patches, indices))
+        image.save(f"{out_dir}/result_{i}.png")
 
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
