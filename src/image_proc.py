efd85cfdad2ad9355deb019c06371ff7a206e4e4
Slevire <syliu1@asu.edu>
2022-04-07 18:47:42 -0700

    altered functions to be usable in loops, removed testing calls

--- src/image_proc.py
+++ src/image_proc.py
@@ -1,11 +1,8 @@
-import numpy as np
 from PIL import Image, ImageChops
 import os
 from skimage import io
 from empatches import EMPatches
 from ast import literal_eval
-from functools import cmp_to_key
-import re
 from natsort import natsorted
 
 def slice_img(img):
@@ -14,24 +11,24 @@ def slice_img(img):
     emp = EMPatches()
     patches, indices = emp.extract_patches(image, patchsize=256, overlap=0.1)
 
-    with open('indices.txt', 'w') as f:
+    with open('indices.txt', 'a+') as f:
         f.write(str(indices))
     
     for i in range(len(patches)):
         im = Image.fromarray(patches[i])
         im.save(f"./Patches/{i}_{os.path.basename(img)}")
 
-def stitch_patches(patch_dir, out_dir):
+def stitch_patches(patch_dir, out_dir, i):
     
     emp = EMPatches()
     patches = [io.imread(patch) for patch in natsorted([os.path.join(patch_dir, patch) for patch in os.listdir(patch_dir)])]
     
     with open('indices.txt') as f:
-        indices = literal_eval(f.readlines()[0])
+        indices = [literal_eval(index) for index in f.readlines()]
     
     im = emp.merge_patches(patches, indices) 
     image = Image.fromarray(im)
-    image.save(f"{out_dir}/result.png")
+    image.save(f"{out_dir}/result_{i}.png")
 
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
@@ -40,8 +37,3 @@ def trim(im):
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
-
-if __name__ == "__main__":
-    #slice_img('Tests/top_mosaic_09cm_area5.png')
-    #stitch_patches('./Patches', '.')
-    stitch_patches('A:/Spring 2022/CSE 598/Background Removal/DeepNetsForEO-master/Patches', '.')
